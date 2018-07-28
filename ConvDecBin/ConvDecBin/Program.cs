90ef31f0e1df1dc9c8c0888e8d9cf8b1220bfd2f
Lucia Amador <luciaap03@gmail.com>
2018-07-28 19:50:40 -0400

    Add files via upload

---
+++ ConvDecBin/ConvDecBin/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvDecBin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, c, k;
+            Console.WriteLine("Ingrese un numero decimal: \n");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("El numero binario es: \n");
+
+            for (c = 13;  c >= 0; c--)
+            {
+                k = n >> c;
+                if (Convert.ToBoolean(k & 1))
+                    Console.Write("1");
+                else
+                    Console.Write("0");
+            }
+            Console.ReadKey();
+        }
+    }
+}
